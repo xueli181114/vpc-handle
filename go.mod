eb614fecec340ea3ec0d3fdbfe3266533573b1d6
Xue Li <xueli@redhat.com>
2024-11-18 14:03:33 +0800

    make a script for vpc clean up

---
+++ go.mod
@@ -0,0 +1,35 @@
+module vpc-handler
+
+go 1.22.5
+
+require github.com/openshift-online/ocm-common v0.0.13
+
+require (
+	github.com/apparentlymart/go-cidr v1.1.0 // indirect
+	github.com/aws/aws-sdk-go-v2 v1.30.3 // indirect
+	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.6.2 // indirect
+	github.com/aws/aws-sdk-go-v2/config v1.27.9 // indirect
+	github.com/aws/aws-sdk-go-v2/credentials v1.17.9 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.16.0 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/configsources v1.3.15 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/endpoints/v2 v2.6.15 // indirect
+	github.com/aws/aws-sdk-go-v2/internal/ini v1.8.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/cloudformation v1.48.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/cloudwatchlogs v1.35.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ec2 v1.152.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/elasticloadbalancingv2 v1.34.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/iam v1.27.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/accept-encoding v1.11.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/internal/presigned-url v1.11.6 // indirect
+	github.com/aws/aws-sdk-go-v2/service/kms v1.30.0 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ram v1.26.1 // indirect
+	github.com/aws/aws-sdk-go-v2/service/route53 v1.40.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sso v1.20.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/ssooidc v1.23.3 // indirect
+	github.com/aws/aws-sdk-go-v2/service/sts v1.28.5 // indirect
+	github.com/aws/smithy-go v1.20.3 // indirect
+	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/sirupsen/logrus v1.9.3 // indirect
+	golang.org/x/crypto v0.22.0 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+)
---
+++ main/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/openshift-online/ocm-common/pkg/aws/aws_client"
+	"github.com/openshift-online/ocm-common/pkg/test/vpc_client"
+)
+
+var region string = "us-west-2"
+
+func CleanUpVPC() {
+	client, err := aws_client.CreateAWSClient("", region)
+	if err != nil {
+		panic(err.Error())
+	}
+	vpcs, err := client.ListVPCs()
+	if err != nil {
+		panic(err.Error())
+	}
+	vpcCleanup := []string{}
+	throttleTime := time.Now().Add(-6 * time.Hour)
+	for _, vpcExist := range vpcs {
+		nameMatch := false
+		timeMatch := false
+		for _, tag := range vpcExist.Tags {
+			if *tag.Key == "Name" && (strings.Contains(*tag.Value, "gate-stg-cs-ci")) { // all gating jobs prepared vpc are started from gate-<env>-cs-ci
+				nameMatch = true
+			}
+			if *tag.Key == "openshift_creationDate" {
+				creationTime, err := time.Parse(time.RFC3339, *tag.Value)
+				if err != nil {
+					panic(err.Error())
+				}
+				if creationTime.Before(throttleTime) {
+					timeMatch = true
+				}
+			}
+		}
+		if nameMatch && timeMatch {
+			fmt.Println(">>> Got one matched vpc need to clean: ", *vpcExist.VpcId)
+			vpcCleanup = append(vpcCleanup, *vpcExist.VpcId)
+		}
+	}
+
+	wg := sync.WaitGroup{}
+	for _, vpcID := range vpcCleanup {
+		wg.Add(1)
+		go func(vpcID string) {
+			defer wg.Done()
+			vpc, err := vpc_client.GenerateVPCByID(vpcID, region)
+			if err != nil {
+				return
+			}
+			err = vpc.DeleteVPCChain(true)
+			if err != nil {
+				fmt.Println(">>>> vpc ID: ", vpc.VpcID)
+			}
+		}(vpcID)
+	}
+
+	wg.Wait()
+
+}
+func FindLauncImage() {
+	vpc, _ := vpc_client.PrepareVPC("xueli", region, "", true, "")
+	imageID, err := vpc.FindProxyLaunchImage()
+	fmt.Println(err)
+	fmt.Println(imageID)
+}
+func LaunchProxy() {
+	vpc, _ := vpc_client.PrepareVPC("xueli", region, "", true, "")
+	defer vpc.DeleteVPCChain(true)
+	_, ip, ca, err := vpc.LaunchProxyInstance(region+"a", "xuelitmp2", "/Users/lixue/Workspace/ocm-common")
+	fmt.Println(ip)
+	fmt.Println(ca)
+	fmt.Println(err)
+}
+
+func main() {
+	CleanUpVPC()
+}
